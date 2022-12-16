f2d2fdf3c8a7d87bfa5bdb7232e44ed029f33e5a
Peng <1a84@proton.me>
2022-12-16 13:51:16 +0100

    fix: status bar on android

--- src/components/UIProvider/UIProvider.js
+++ src/components/UIProvider/UIProvider.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import { MD3DarkTheme, MD3LightTheme, Provider as PaperProvider } from 'react-native-paper';
+import {
+  MD3DarkTheme,
+  MD3LightTheme,
+  Provider as PaperProvider,
+  useTheme,
+} from 'react-native-paper';
 
 export default function UIProvider({ children, themeMode }) {
+  const theme = useTheme();
+  const isLight = themeMode === 'light';
   return (
-    <PaperProvider theme={themeMode === 'light' ? MD3LightTheme : MD3DarkTheme}>
-      <StatusBar barStyle={themeMode === 'light' ? 'dark-content' : 'light-content'} />
+    <PaperProvider theme={isLight ? MD3LightTheme : MD3DarkTheme}>
+      <StatusBar
+        backgroundColor={isLight ? theme.colors.background : theme.colors.onBackground}
+        barStyle={isLight ? 'dark-content' : 'light-content'}
+      />
       {children}
     </PaperProvider>
   );
