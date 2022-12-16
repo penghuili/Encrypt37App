78e6b1c76cc31463e4077e770d74fa1bd8e44bff
Peng <1a84@proton.me>
2022-12-16 12:18:01 +0100

    fix: not set bg color for status bar

--- src/components/UIProvider/UIProvider.js
+++ src/components/UIProvider/UIProvider.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import {
-  MD3DarkTheme,
-  MD3LightTheme,
-  Provider as PaperProvider,
-  useTheme,
-} from 'react-native-paper';
+import { MD3DarkTheme, MD3LightTheme, Provider as PaperProvider } from 'react-native-paper';
 
 export default function UIProvider({ children, themeMode }) {
-  const theme = useTheme();
-
   return (
     <PaperProvider theme={themeMode === 'light' ? MD3LightTheme : MD3DarkTheme}>
       <StatusBar
-        backgroundColor={theme.colors.onBackground}
         barStyle={themeMode === 'light' ? 'dark-content' : 'light-content'}
         translucent={false}
       />
